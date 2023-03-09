54bbe4458734088f7762b5cd2e51f497ddc3640a
fedezam <samprasfed@gmail.com>
2023-03-08 23:13:35 -0300

    initial commit

---
+++ curriculum.js
@@ -0,0 +1,53 @@
+const formulario = document.querySelector('#formulario');
+const nombre = document.querySelector('#nombre');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+const enviar = document.querySelector('#enviar');
+const errores = document.querySelector('#errores');
+let mensajesErrores = [];
+
+
+function validar (evento) {
+    
+    evento.preventDefault();
+
+    // Vacia los mensajesErrores antes de rellenarlo nuevamente
+    mensajesErrores = [];
+
+    // VALIDACIONES
+
+    if (nombre.value.trim().length === 0) {
+        mensajesErrores = mensajesErrores.concat('Nombre es un campo obligatorio');
+    }
+
+    if (!/^[a-zA-Z0-9]*$/.exec(nombre.value.trim())) {
+        mensajesErrores = mensajesErrores.concat('Nombre no tiene carácteres válidos');
+    }
+
+    
+
+    // Comprobamos que el mensaje tiene un mínimo de 10 carácteres
+
+    if (mensaje.value.trim().length < 10) {
+        mensajesErrores = mensajesErrores.concat('Mensaje demasiado corto');
+    }
+
+    // ENVIAR O MOSTRAR MENSAJES
+    if (mensajesErrores.length === 0) {
+        // Enviamos el formulario si no hay errores
+        formulario.submit();
+    } else {
+        // Muestro los errores
+        errores.textContent = '';
+        mensajesErrores.forEach(function (mensaje) {
+            const miLi = document.createElement('li');
+            miLi.textContent = mensaje;
+            errores.appendChild(miLi);
+        });
+    }
+}
+
+// Eventos
+formulario.addEventListener('submit', validar);
+
+// Inicio
